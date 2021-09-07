a733ea19f5b4200285f44601c5e9010e2c34fd19
RachmanForniandi <rachmanforniandi@gmail.com>
2021-09-07 08:33:37 +0700

    initial commit. Add data place page done.

---
+++ app/src/main/java/rachman/forniandi/galleryplaces/AddPlaceActivity.kt
@@ -0,0 +1,11 @@
+package rachman.forniandi.galleryplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_place)
+    }
+}
